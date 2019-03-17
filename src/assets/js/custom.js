f4cad8f3b6a98aa211f000c934c7684f2da455fa
STCServiciosDiaz <stcserviciosdiaz@outlook.com>
2019-03-17 13:41:28 -0400

    ready view gallery

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,4 +1,26 @@
 jQuery(document).ready(function() {
+
+    /*** ** Funtion view Gallery*****/
+    $(document).ready(function(){
+        $(".filter-button").click(function(){
+            alert('eeee');
+            var value = $(this).attr('data-filter');            
+            if(value == "all")
+            {
+                $('.filter').show('1000');
+            }
+            else
+            {
+                $(".filter").not('.'+value).hide('3000');
+                $('.filter').filter('.'+value).show('3000');
+                
+            }
+        });
+        if ($(".filter-button").removeClass("active")) {
+            $(this).removeClass("active");
+        }
+        $(this).addClass("active");    
+    });
   
 
    $('#carouselHacked').carousel();
